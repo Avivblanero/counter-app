b951bb3fa88aa6e843436c0f5bdcc9ed219fc7bf
Avivblanero <avivblanero@gmail.com>
2022-01-25 17:54:52 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import react from 'react';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+
+
+function App() {
+
+  const [count, setCount] = useState(10);
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    if (count === 0) {
+      setIsHidden = true;
+    }
+  }, [count])
+
+  return (
+    <div className="App">
+      <p>{count}</p>
+      {isHidden ? <p>You can no longer decrease the counter</p> : <button onClick={() => setCount(count - 1)}>click me!</button>}
+    </div >
+  );
+}
+
+export default App;
